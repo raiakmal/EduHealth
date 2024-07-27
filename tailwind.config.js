d04077e2ab6513f6f77bce2c84b0af5c0d066b54
Rai Akmal <muhammadrai0504@gmail.com>
2024-07-27 13:01:46 +0700

    update

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto Flex', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+        edu: ['Edu AU VIC WA NT Hand', 'cursive'],
+      },
+      colors: {
+        birumuda: '#E5FFF8',
+        birutua: '#01649C',
+      },
+    },
+  },
+  plugins: [],
+};
